19c96a3a66b4275b49fbc4b42e9cdc49fee8d406
Andre Teixeira da Silva <devAndretds@outlook.com>
2021-09-12 01:07:36 -0300

    ajuste na modal de consultar empresa

--- src/components/consulta/consulta-empresa.jsx
+++ src/components/consulta/consulta-empresa.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Modal, ModalBody, Row, InputGroup, Button, FormControl, Form } from 'react-bootstrap'
+import ReactDOM from 'react-dom'
 import ModalHeader from 'react-bootstrap/esm/ModalHeader'
 
 const ConsultaEmpresa = () => {
@@ -10,35 +11,41 @@ const ConsultaEmpresa = () => {
     const handlerConsultar = (event) => {
         event.preventDefault()
     }
+
+    const modalOverlayElement =  document.getElementById('overlay-root')
+
     return (
         <>
-            <Modal show={show} onHide={handleClose} size="lg">
-                <ModalHeader closeButton>
-                    <Modal.Title id="example-modal-sizes-title-sm">
-                        Selecionar Empresa
-                    </Modal.Title>
-                </ModalHeader>
-                <ModalBody>
-                    <Row>
-                        <Form onSubmit={handlerConsultar}>
-                            <InputGroup className="mb-3">
-                                <FormControl
-                                    aria-label="Recipient's username"
-                                    aria-describedby="basic-addon2"
-                                />
-                                <Button type='submit' variant="outline-secondary" id="btnConsultar">
-                                    Consultar
-                                </Button>
-                            </InputGroup>
-                        </Form>
-                    </Row>
-                </ModalBody>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={handleClose}>
-                        OK
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+            {
+                ReactDOM.createPortal(
+                <Modal show={show} onHide={handleClose} size="lg" backdrop='static'>
+                    <ModalHeader closeButton>
+                        <Modal.Title id="example-modal-sizes-title-sm">
+                            Selecionar Empresa
+                        </Modal.Title>
+                    </ModalHeader>
+                    <ModalBody>
+                        <Row>
+                            <Form onSubmit={handlerConsultar}>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                        aria-label="Recipient's username"
+                                        aria-describedby="basic-addon2"
+                                    />
+                                    <Button type='submit' variant="outline-secondary" id="btnConsultar">
+                                        Consultar
+                                    </Button>
+                                </InputGroup>
+                            </Form>
+                        </Row>
+                    </ModalBody>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={handleClose}>
+                            OK
+                        </Button>
+                    </Modal.Footer>
+                </Modal>, modalOverlayElement)
+            }
         </>
     )
 }
